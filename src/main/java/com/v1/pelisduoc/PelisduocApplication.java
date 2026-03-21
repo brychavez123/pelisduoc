fa7e0b476fb5b6833f84f2214cf3b65cdef5830a
bryan <bryanandres9@hotmail.com>
2026-03-21 15:01:45 -0600

    Estructura Inicial

---
+++ src/main/java/com/v1/pelisduoc/PelisduocApplication.java
@@ -0,0 +1,13 @@
+package com.v1.pelisduoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelisduocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelisduocApplication.class, args);
+	}
+
+}
